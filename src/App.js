e2c8d5a7e69b8221e08b2f1d7d5098b2f030182a
aehaq <azfarehaq@gmail.com>
2018-09-30 05:32:30 -0700

    app organized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import options from "./options.json";
 import './App.css';
 
 class App extends Component {
+  // Options array
+  state = {
+    options
+  };
+
+  score = 0;
+
+  topScore = 0;
+
+  randomizeOrder = () => {
+    //change order of this.state.options
+  }
+
+  handleClick = id => {
+    //handle click
+  }
+
   render() {
     return (
-      <p></p>
+      <Wrapper>
+        <Header score={this.score} topScore={this.topScore} />
+        <Jumbotron />
+        {this.state.options.map(option => (
+        <GameCards 
+          id={option.id}
+          key={option.id}
+          image={option.image}
+        />
+        ))}
+      </Wrapper>
     );
   }
 }
